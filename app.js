edeff18303a5fa0740797c54e8c3313f4a6c7a63
Teo Diaz <teo.diaz@cabify.com>
2020-12-26 00:42:03 +0100

    Add Procfile

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-const { App } = require('@slack/bolt');
-console.log(process.env.SLACK_BOT_TOKEN)
+const { App } = require("@slack/bolt");
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+          },
+          action_id: "button_click",
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
+});
+
+app.action("button_click", async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 8000;
+  }
 
-  console.log('⚡️ Bolt app is running!');
+  await app.start(port);
+
+  console.log("⚡️ Bolt app is running!");
 })();
