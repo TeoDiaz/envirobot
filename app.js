04dbec01e249abc00a789fb503975bc87c872816
Teo Diaz <teo.diaz@cabify.com>
2020-12-26 10:09:47 +0100

    create action button

--- app.js
+++ app.js
@@ -2,182 +2,167 @@ const { App } = require("@slack/bolt");
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-
-let help3 = "teo.diaz"
-let remedios3 = "francis.perez"
+let help3 = "";
+let remedios3 = "";
+let help4 = "";
+let remedios4 = "";
 
 // Listens to incoming messages that contain "hello"
-app.message("start", async({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    await say({
-        blocks: [{
-                type: "header",
-                text: {
-                    type: "plain_text",
-                    text: "Environments Queue",
-                    emoji: true,
-                },
-            },
-            {
-                type: "section",
-                fields: [{
-                        type: "mrkdwn",
-                        text: "*Support-3*",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "*Owner:*",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "HelpInApp:",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: help3,
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "Remedios:",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: remedios3,
-                    },
-                ],
-            },
-            {
-                type: "section",
-                text: {
-                    type: "mrkdwn",
-                    text: "Select a project to owe",
-                },
-                accessory: {
-                    type: "static_select",
-                    placeholder: {
-                        type: "plain_text",
-                        text: "Select an item",
-                        emoji: true,
-                    },
-                    options: [{
-                            text: {
-                                type: "plain_text",
-                                text: "*this is plain_text text*",
-                                emoji: true,
-                            },
-                            value: "value-0",
-                        },
-                        {
-                            text: {
-                                type: "plain_text",
-                                text: "*this is plain_text text*",
-                                emoji: true,
-                            },
-                            value: "value-1",
-                        },
-                        {
-                            text: {
-                                type: "plain_text",
-                                text: "*this is plain_text text*",
-                                emoji: true,
-                            },
-                            value: "value-2",
-                        },
-                    ],
-                    action_id: "static_select-action",
-                },
-            },
-            {
-                type: "section",
-                fields: [{
-                        type: "mrkdwn",
-                        text: "*Support-4*",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "*Owner:*",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "HelpInApp:",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "teo.diaz",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "Remedios:",
-                    },
-                    {
-                        type: "mrkdwn",
-                        text: "francis.perez",
-                    },
-                ],
-            },
-            {
-                type: "section",
-                text: {
-                    type: "mrkdwn",
-                    text: "Select a project to owe",
-                },
-                accessory: {
-                    type: "static_select",
-                    placeholder: {
-                        type: "plain_text",
-                        text: "Select an item",
-                        emoji: true,
-                    },
-                    options: [{
-                            text: {
-                                type: "plain_text",
-                                text: "*this is plain_text text*",
-                                emoji: true,
-                            },
-                            value: "value-0",
-                        },
-                        {
-                            text: {
-                                type: "plain_text",
-                                text: "*this is plain_text text*",
-                                emoji: true,
-                            },
-                            value: "value-1",
-                        },
-                        {
-                            text: {
-                                type: "plain_text",
-                                text: "*this is plain_text text*",
-                                emoji: true,
-                            },
-                            value: "value-2",
-                        },
-                    ],
-                    action_id: "static_select-action",
-                },
-            },
+app.message("start", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Environments Queue",
+          emoji: true,
+        },
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Support-3*",
+          },
+          {
+            type: "mrkdwn",
+            text: "*Owner:*",
+          },
+          {
+            type: "mrkdwn",
+            text: "HelpInApp:",
+          },
+          {
+            type: "mrkdwn",
+            text: help3,
+          },
+          {
+            type: "mrkdwn",
+            text: "Remedios:",
+          },
+          {
+            type: "mrkdwn",
+            text: remedios3,
+          },
         ],
-    });
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Select a project to owe",
+        },
+        accessory: {
+          type: "static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select an item",
+          },
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "HelpInApp",
+              },
+              value: "value-0",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Remedios",
+              },
+              value: "value-1",
+            },
+          ],
+          action_id: "select-support-3",
+        },
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Support-4*",
+          },
+          {
+            type: "mrkdwn",
+            text: "*Owner:*",
+          },
+          {
+            type: "mrkdwn",
+            text: "HelpInApp:",
+          },
+          {
+            type: "mrkdwn",
+            text: help4,
+          },
+          {
+            type: "mrkdwn",
+            text: "Remedios:",
+          },
+          {
+            type: "mrkdwn",
+            text: remedios4,
+          },
+        ],
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Select a project to owe",
+        },
+        accessory: {
+          type: "static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select an item",
+          },
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "HelpInApp",
+              },
+              value: "value-0",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Remedios4",
+                emoji: true,
+              },
+              value: "value-1",
+            }
+          ],
+          action_id: "select-support-4",
+        },
+      },
+    ],
+  });
 });
 
-app.action("button_click", async({ body, ack, say }) => {
-    // Acknowledge the action
-    await ack();
-    await say(`<@${body.user.id}> clicked the button`);
+app.action("select-support-3", async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
 });
 
-(async() => {
-    // Start your app
-    let port = process.env.PORT;
-    if (port == null || port == "") {
-        port = 8000;
-    }
+(async () => {
+  // Start your app
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 8000;
+  }
 
-    await app.start(port);
+  await app.start(port);
 
-    console.log("⚡️ Bolt app is running!");
+  console.log("⚡️ Bolt app is running!");
 })();
