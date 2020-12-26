533630442980bfcc6adee10d99cebe3103cee844
Teo Diaz <teo.diaz@cabify.com>
2020-12-26 09:56:12 +0100

    trying things

--- app.js
+++ app.js
@@ -6,28 +6,149 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+let help3 = "teo.diaz"
+let remedios3 = "francis.perez"
+
 // Listens to incoming messages that contain "hello"
-app.message("hello", async ({ message, say }) => {
+app.message("start", async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   await say({
     blocks: [
       {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Environments Queue",
+          emoji: true,
+        },
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Support-3*",
+          },
+          {
+            type: "mrkdwn",
+            text: "*Owner:*",
+          },
+          {
+            type: "mrkdwn",
+            text: "HelpInApp:",
+          },
+          {
+            type: "mrkdwn",
+            text: {help3},
+          },
+          {
+            type: "mrkdwn",
+            text: "Remedios:",
+          },
+          {
+            type: "mrkdwn",
+            text: {remedios3},
+          },
+        ],
+      },
+      {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `Hey there <@${message.user}>!`,
+          text: "Select a project to owe",
         },
         accessory: {
-          type: "button",
-          text: {
+          type: "static_select",
+          placeholder: {
             type: "plain_text",
-            text: "Click Me",
+            text: "Select a project",
+            emoji: true,
           },
-          action_id: "button_click",
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "HelpInApp",
+                emoji: true,
+              },
+              value: "HelpInApp3",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Remedios",
+                emoji: true,
+              },
+              value: "Remedios3",
+            },
+          ],
+          action_id: "selection-support-3",
+        },
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Support-4*",
+          },
+          {
+            type: "mrkdwn",
+            text: "*Owner:*",
+          },
+          {
+            type: "mrkdwn",
+            text: "HelpInApp:",
+          },
+          {
+            type: "mrkdwn",
+            text: "teo.diaz",
+          },
+          {
+            type: "mrkdwn",
+            text: "Remedios:",
+          },
+          {
+            type: "mrkdwn",
+            text: "francis.perez",
+          },
+        ],
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Select a project to owe",
+        },
+        accessory: {
+          type: "static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select a project",
+            emoji: true,
+          },
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "HelpInApp",
+                emoji: true,
+              },
+              value: "HelpInApp4",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Remedios",
+                emoji: true,
+              },
+              value: "Remedios4",
+            },
+          ],
+          action_id: "selection-support-4",
         },
       },
     ],
-    text: `Hey there <@${message.user}>!`,
   });
 });
 
