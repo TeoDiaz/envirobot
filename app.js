f7ae17b0ca8f14359a5d31431ac984117bcb2389
Teo Diaz <teo.diaz@cabify.com>
2020-12-27 01:10:30 +0100

    try

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ let section = () => {
           },
           {
             type: "mrkdwn",
-            text: environments.help3,
+            text: environments.help3.join(' '),
           },
           {
             type: "mrkdwn",
@@ -49,7 +49,7 @@ let section = () => {
           },
           {
             type: "mrkdwn",
-            text: environments.remedios3,
+            text: environments.remedios3.join(' '),
           },
         ],
       },
