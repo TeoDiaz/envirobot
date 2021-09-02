9a3590947df60d088c4a51d5f820ccd7e3bff0e8
Teo Diaz <teo.diaz@cabify.com>
2021-09-02 17:41:03 +0200

    remove files no needed

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# vim: ai:ts=8:sw=8:noet
-set -eufo pipefail
-export SHELLOPTS	# propagate set to children by default
-IFS=$'\t\n'
-# shellcheck disable=SC1090
-source /usr/local/lib/functionarium/cabify_rfc_docker_tag.sh
-version="$(cabify_rfc_docker_tag)"
-/usr/bin/kapsule-deploy "${REPOSITORY}" "${DEPLOYMENT}" "${version}"
